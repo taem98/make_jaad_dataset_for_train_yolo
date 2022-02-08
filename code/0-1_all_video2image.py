43db34e1565107e5b1b78c18c5570b0708df5f02
taem98 <taemi@chungbuk.ac.kr>
2022-02-08 13:37:24 +0900

    change first frame from 0

--- code/0-1_all_video2image.py
+++ code/0-1_all_video2image.py
@@ -7,11 +7,12 @@ def video_to_frames(video, path_output_dir):
     vidcap = cv2.VideoCapture(video)
     count = 0
     while vidcap.isOpened():
+        
         success, image = vidcap.read()
         if success:
-            count += 1
             cv2.imwrite(path_output_dir+'{}.png'.format(str(count).zfill(4)), image)
-            
+            count += 1
+
         else:
             break
     cv2.destroyAllWindows()
